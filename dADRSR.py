dac0dc3f3c1935dded823d39c571d925c0bd9bbf
AlisaKugusheva <74014280+AlisaKugusheva@users.noreply.github.com>
2022-06-16 17:02:33 +0200

    Update dADRSR.py

--- dADRSR.py
+++ dADRSR.py
@@ -1,124 +1,100 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
 
 
-def SE_block(input_tensor, r=10):
-    x = input_tensor
-    bs = x.shape[0]
-    n_channels = x.shape[1]
+def conv3x3(in_planes, out_planes, dilation):
     
-    x = nn.AvgPool2d((x.shape[2], x.shape[3]))(x)
-    x = x.view(bs, n_channels)
-    x = nn.Linear(n_channels, n_channels // r)(x)
-    x = nn.ReLU()(x)
-    x = nn.Linear(n_channels // r, n_channels)(x)
-    x = torch.sigmoid(x)
-    x = x.reshape(bs, n_channels, 1, 1)
-    
-    return input_tensor * x
+    return nn.Conv2d(
+        in_channels=in_planes,
+        out_channels=out_planes,
+        kernel_size=3,
+        stride=1,
+        padding='same',
+        dilation=dilation,
+        bias=False
+     )
 
-
-def M_ARB(input_tensor, n_filters_1=120, n_filters_2=10):
-    x = input_tensor
-    n_channels = x.shape[1]
-    
-    x1 = nn.Conv2d(n_channels, n_filters_1, kernel_size=3, stride=1, padding='same', dilation=1)(x)
-    x1 = nn.ReLU()(x1)
-    x1 = nn.Conv2d(n_filters_1, n_filters_2, kernel_size=3, stride=1, padding='same', dilation=1)(x1)
-    
-    x2 = nn.Conv2d(n_channels, n_filters_1, kernel_size=3, stride=1, padding='same', dilation=3)(x)
-    x2 = nn.ReLU()(x2)
-    x2 = nn.Conv2d(n_filters_1, n_filters_2, kernel_size=3, stride=1, padding='same', dilation=3)(x2)
-    
-    x3 = nn.Conv2d(n_channels, n_filters_1, kernel_size=3, stride=1, padding='same', dilation=5)(x)
-    x3 = nn.ReLU()(x3)
-    x3 = nn.Conv2d(n_filters_1, n_filters_2, kernel_size=3, stride=1, padding='same', dilation=5)(x3)
-    
-    x = torch.cat([x1, x2, x3], axis=1)
-    x = SE_block(x)
-    
-    return x + input_tensor
-
-
-class dADR_SR(nn.Module):
-    def __init__(self, n_channels=4, n_filters=10, n_blocks=12, n_filters_1=120, n_filters_2=10):
+class SEBlock(nn.Module):
+    def __init__(self, H, W, n_channels=30, r=10):
         super().__init__()
         self.n_channels = n_channels
-        self.n_filters = n_filters
-        self.n_blocks = n_blocks
-        self.n_filters_1 = n_filters_1
-        self.n_filters_2 = n_filters_2
-        
-        self.conv_first_dil_1 = nn.Conv2d(in_channels=self.n_channels, 
-                       out_channels=self.n_filters, 
-                       kernel_size=3, 
-                       stride=1, 
-                       padding='same', 
-                       dilation=1)
-        self.conv_first_dil_3 = nn.Conv2d(in_channels=self.n_channels, 
-                       out_channels=self.n_filters, 
-                       kernel_size=3, 
-                       stride=1, 
-                       padding='same', 
-                       dilation=3)
-        self.conv_first_dil_5 = nn.Conv2d(in_channels=self.n_channels, 
-                       out_channels=self.n_filters, 
-                       kernel_size=3, 
-                       stride=1, 
-                       padding='same', 
-                       dilation=5)
-        
-        self.M_ARB_dil_1 = nn.Sequential(
-                                      nn.Conv2d(n_filters * 3, n_filters_1, 
-                                                kernel_size=3, stride=1, 
-                                                padding='same', dilation=1),
-                                      nn.ReLU(),
-                                      nn.Conv2d(n_filters_1, n_filters_2, 
-                                                kernel_size=3, stride=1, 
-                                                padding='same', dilation=1)
+        self.avgpool = nn.AvgPool2d((H, W))
+        self.excitation = nn.Sequential(
+            nn.Linear(n_channels, n_channels // r),
+            nn.ReLU(True),
+            nn.Linear(n_channels // r, n_channels)
         )
+    def forward(self, x):
+        input_tensor = x
+        bs = x.shape[0]
         
-        self.M_ARB_dil_3 = nn.Sequential(
-                                      nn.Conv2d(n_filters * 3, n_filters_1, 
-                                                kernel_size=3, stride=1, 
-                                                padding='same', dilation=3),
-                                      nn.ReLU(),
-                                      nn.Conv2d(n_filters_1, n_filters_2, 
-                                                kernel_size=3, stride=1, 
-                                                padding='same', dilation=3)
+        x = self.avgpool(x)
+        x = x.view(bs, -1)
+        x = self.excitation(x)
+        X = torch.sigmoid(x)
+        x = x.reshape(bs, self.n_channels, 1, 1)
+        
+        return input_tensor * x
+    
+class MARBBlock(nn.Module):
+    def __init__(self, in_channels, H, W, n_filters_1=120, n_filters_2=10):
+        super().__init__()
+        self.branch_1 = nn.Sequential(
+            conv3x3(in_channels, n_filters_1, dilation=1),
+            nn.ReLU(True),
+            conv3x3(n_filters_1, n_filters_2, dilation=1)
         )
-        
-        self.M_ARB_dil_5 = nn.Sequential(
-                                      nn.Conv2d(n_filters * 3, n_filters_1, 
-                                                kernel_size=3, stride=1, 
-                                                padding='same', dilation=5),
-                                      nn.ReLU(),
-                                      nn.Conv2d(n_filters_1, n_filters_2, 
-                                                kernel_size=3, stride=1, 
-                                                padding='same', dilation=5)
+        self.branch_2 = nn.Sequential(
+            conv3x3(in_channels, n_filters_1, dilation=3),
+            nn.ReLU(True),
+            conv3x3(n_filters_1, n_filters_2, dilation=3)
         )
+        self.branch_3 = nn.Sequential(
+            conv3x3(in_channels, n_filters_1, dilation=5),
+            nn.ReLU(True),
+            conv3x3(n_filters_1, n_filters_2, dilation=5)
+        )
+        self.se = SEBlock(H, W)
+    
+    def forward(self, x):
+        input_tensor = x
         
-    def forward(self, x):       
-        x1 = self.conv1(x)
-        x2 = self.conv3(x)
-        x3 = self.conv5(x)
-        x = x_inp = torch.cat([x1, x2, x3], axis=1)
+        x1 = self.branch_1(x)
+        x2 = self.branch_2(x)
+        x3 = self.branch_3(x)
         
-        x1 = self.M_ARB_dil_1(x)
-        x2 = self.M_ARB_dil_3(x)
-        x3 = self.M_ARB_dil_5(x)
-            
-        x = nn.Conv2d(in_channels=x.shape[1],
-                      out_channels=x.shape[1],
-                      kernel_size=3,
-                      padding='same')(x)
+        out = torch.cat([x1, x2, x3], axis=1)
+        out = self.se(out)
+        out += input_tensor
         
-        x = x_inp + x
-        x = nn.Conv2d(in_channels=x.shape[1],
-                      out_channels=1,
-                      kernel_size=3,
-                      padding='same')(x)
+        return out
+    
+class dADR_SR(nn.Module):
+    def __init__(self, in_channels, H, W, n_blocks=12, n_filters_1=120, n_filters_2=10):
+        super().__init__()
+        self.conv1_1 = conv3x3(in_channels, 10, dilation=1)
+        self.conv1_2 = conv3x3(in_channels, 10, dilation=3)
+        self.conv1_3 = conv3x3(in_channels, 10, dilation=5)
+        self.blocks = self._make_layer(30, H, W, 120, 10, 12)
+        self.conv2 = conv3x3(30, 30, dilation=1)
+        self.conv3 = conv3x3(30, 1, dilation=1)
         
-        return x
+    def _make_layer(self, in_channels, H, W, n_filters_1, n_filters_2, blocks):
+        layers = []
+        for _ in range(blocks):
+            layers.append(MARBBlock(in_channels, H, W, n_filters_1, n_filters_2))
+        
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x1 = self.conv1_1(x)
+        x2 = self.conv1_2(x)
+        x3 = self.conv1_3(x)
+        x = torch.cat([x1, x2, x3], axis=1)
+        
+        out = self.blocks(x)
+        out = self.conv2(out)
+        out += x
+        out = self.conv3(out)
+        
+        return out
